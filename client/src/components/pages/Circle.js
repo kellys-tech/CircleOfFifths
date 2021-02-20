28449ba04b99ca5ae9bb14b2f65ea0670c5189bb
Kelly Smith <kdalton77@hotmail.com>
2021-02-20 16:18:37 -0500

    added piano functionality

--- client/src/components/pages/Circle.js
+++ client/src/components/pages/Circle.js
@@ -1,59 +1,61 @@
 import React, { useContext } from 'react';
 // import { Store } from '../../store';
 // import { Link } from 'react-router-dom';
+// for submit button when implemented
+// import Button from '@material-ui/core/Button';
 
 const CirclePage = () => {
-
 
 return (
 <div>
 <h1>The Circle of Fifths</h1>
-    <div class="container-fluid">
-        <div class="row">
-            <div class= "col-3"></div>
-            <div class="col-6 keyboardCol">
+    {/* <div className="container-fluid"> */}
+    <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+        <div className="row">
+            <div className= "col-3"></div>
+            <div className="col-6 keyboardCol">
                 {/* <img src="./assets/keyboardImage.png" alt="" id="keyboardImage"> */}
-                <div class="keyboardContainer">
-                    <div class="whiteKeys">
-                        <button class="pianoKeys white" id="one" data-pitch="C" data-alt="B#"></button>
-                        <button class="pianoKeys white" id="two" data-pitch="D"></button>
-                        <button class="pianoKeys white" id="three" data-pitch="E" data-alt="Fb"></button>
-                        <button class="pianoKeys white" id="four" data-pitch="F" data-alt="E#"></button>
-                        <button class="pianoKeys white" id="five" data-pitch="G"></button>
-                        <button class="pianoKeys white" id="six" data-pitch="A"></button>
-                        <button class="pianoKeys white" id="seven" data-pitch="B" data-alt="Cb"></button>
-                        <button class="pianoKeys white" id="eight" data-pitch="C" data-alt="B#"></button>
-                        <button class="pianoKeys white" id="nine" data-pitch="D"></button>
-                        <button class="pianoKeys white" id="ten" data-pitch="E" data-alt="Fb"></button>
-                        <button class="pianoKeys white" id="eleven" data-pitch="F" data-alt="E#"></button>
-                        <button class="pianoKeys white" id="twelve" data-pitch="G"></button>
-                        <button class="pianoKeys white" id="thirteen" data-pitch="A"></button>
-                        <button class="pianoKeys white" id="fourteen" data-pitch="B" data-alt="Cb"></button>
+                <div className="keyboardContainer">
+                    <div className="whiteKeys">
+                        <button className="pianoKeys white" id="one" data-pitch="C" data-alt="B#"></button>
+                        <button className="pianoKeys white" id="two" data-pitch="D"></button>
+                        <button className="pianoKeys white" id="three" data-pitch="E" data-alt="Fb"></button>
+                        <button className="pianoKeys white" id="four" data-pitch="F" data-alt="E#"></button>
+                        <button className="pianoKeys white" id="five" data-pitch="G"></button>
+                        <button className="pianoKeys white" id="six" data-pitch="A"></button>
+                        <button className="pianoKeys white" id="seven" data-pitch="B" data-alt="Cb"></button>
+                        <button className="pianoKeys white" id="eight" data-pitch="C" data-alt="B#"></button>
+                        <button className="pianoKeys white" id="nine" data-pitch="D"></button>
+                        <button className="pianoKeys white" id="ten" data-pitch="E" data-alt="Fb"></button>
+                        <button className="pianoKeys white" id="eleven" data-pitch="F" data-alt="E#"></button>
+                        <button className="pianoKeys white" id="twelve" data-pitch="G"></button>
+                        <button className="pianoKeys white" id="thirteen" data-pitch="A"></button>
+                        <button className="pianoKeys white" id="fourteen" data-pitch="B" data-alt="Cb"></button>
                     </div>
-                    <div class="blackKeys">
-                        <button class="pianoKeys black" id="fifteen" data-pitch="C#" data-alt="Db"></button>
-                        <button class="pianoKeys black" id="sixteen" data-pitch="D#" data-alt="Eb"></button>
-                        <button class="pianoKeys black" id="seventeen" data-pitch="F#" data-alt="Gb"></button>
-                        <button class="pianoKeys black" id="eighteen" data-pitch="G#" data-alt="Ab"></button>
-                        <button class="pianoKeys black" id="nineteen" data-pitch="A#" data-alt="Bb"></button>
-                        <button class="pianoKeys black" id="twenty" data-pitch="C#" data-alt="Db"></button>
-                        <button class="pianoKeys black" id="twentyOne" data-pitch="D#" data-alt="Eb"></button>
-                        <button class="pianoKeys black" id="twentyTwo" data-pitch="F#" data-alt="Gb"></button>
-                        <button class="pianoKeys black" id="twentyThree" data-pitch="G#" data-alt="Ab"></button>
-                        <button class="pianoKeys black" id="twentyFour" data-pitch="A#" data-alt="Bb"></button>
+                    <div className="blackKeys">
+                        <button className="pianoKeys black" id="fifteen" data-pitch="C#" data-alt="Db"></button>
+                        <button className="pianoKeys black" id="sixteen" data-pitch="D#" data-alt="Eb"></button>
+                        <button className="pianoKeys black" id="seventeen" data-pitch="F#" data-alt="Gb"></button>
+                        <button className="pianoKeys black" id="eighteen" data-pitch="G#" data-alt="Ab"></button>
+                        <button className="pianoKeys black" id="nineteen" data-pitch="A#" data-alt="Bb"></button>
+                        <button className="pianoKeys black" id="twenty" data-pitch="C#" data-alt="Db"></button>
+                        <button className="pianoKeys black" id="twentyOne" data-pitch="D#" data-alt="Eb"></button>
+                        <button className="pianoKeys black" id="twentyTwo" data-pitch="F#" data-alt="Gb"></button>
+                        <button className="pianoKeys black" id="twentyThree" data-pitch="G#" data-alt="Ab"></button>
+                        <button className="pianoKeys black" id="twentyFour" data-pitch="A#" data-alt="Bb"></button>
                     </div>             
                 </div>
             </div>
-            <div class= "col-3"></div>
+            <div className= "col-3"></div>
         </div>
-        <div class="row">
-            <div class="col-4 fsTitle">Flat Scales</div>
-            <div class="col-4 keyOC">C D E F G A B C </div>
-            <div class="col-4 ssTitle">Sharp Scales</div>
+        <div className="row">
+            <div className="col-4 fsTitle">Flat Scales</div>
+            <div className="col-4 keyOC">C D E F G A B C </div>
+            <div className="col-4 ssTitle">Sharp Scales</div>
         </div>
-        <div class="row">
-            <div class="col-4">
-                <div class="flatScales">
+        <div className="row">
+            <div className="col-4">
+                <div className="flatScales">
                     <input type="text" id="fsd1">
                     <input type="text" id="fsd2">
                     <input type="text" id="fsd3">
@@ -63,7 +65,7 @@ return (
                     <input type="text" id="fsd7">
                     <input type="text" id="fsd8">
                 </div>
-                <div class="flatScales">
+                <div className="flatScales">
                     <input type="text" id="fsd1">
                     <input type="text" id="fsd2">
                     <input type="text" id="fsd3">
@@ -73,7 +75,7 @@ return (
                     <input type="text" id="fsd7">
                     <input type="text" id="fsd8">
                 </div>
-                <div class="flatScales">
+                <div className="flatScales">
                     <input type="text" id="fsd1">
                     <input type="text" id="fsd2">
                     <input type="text" id="fsd3">
@@ -83,7 +85,7 @@ return (
                     <input type="text" id="fsd7">
                     <input type="text" id="fsd8">
                 </div>
-                <div class="flatScales">
+                <div className="flatScales">
                     <input type="text" id="fsd1">
                     <input type="text" id="fsd2">
                     <input type="text" id="fsd3">
@@ -93,7 +95,7 @@ return (
                     <input type="text" id="fsd7">
                     <input type="text" id="fsd8">
                 </div>
-                <div class="flatScales">
+                <div className="flatScales">
                     <input type="text" id="fsd1">
                     <input type="text" id="fsd2">
                     <input type="text" id="fsd3">
@@ -103,7 +105,7 @@ return (
                     <input type="text" id="fsd7">
                     <input type="text" id="fsd8">
                 </div>
-                <div class="flatScales">
+                <div className="flatScales">
                     <input type="text" id="fsd1">
                     <input type="text" id="fsd2">
                     <input type="text" id="fsd3">
@@ -113,7 +115,7 @@ return (
                     <input type="text" id="fsd7">
                     <input type="text" id="fsd8">
                 </div>
-                <div class="flatScales">
+                <div className="flatScales">
                     <input type="text" id="fsd1">
                     <input type="text" id="fsd2">
                     <input type="text" id="fsd3">
@@ -124,11 +126,11 @@ return (
                     <input type="text" id="fsd8">
                 </div>
             </div>
-            <div class="col-4">
+            <div className="col-4">
                 <img src="./assets/circleImage1.png" alt="" id="circleImage">
             </div>
-            <div class="col-4">
-                <div class="sharpScales">
+            <div className="col-4">
+                <div className="sharpScales">
                     <input type="text" id="ssd1">
                     <input type="text" id="ssd2">
                     <input type="text" id="ssd3">
@@ -138,7 +140,7 @@ return (
                     <input type="text" id="ssd7">
                     <input type="text" id="ssd8">
                 </div>
-                <div class="sharpScales">
+                <div className="sharpScales">
                     <input type="text" id="ssd11">
                     <input type="text" id="ssd21">
                     <input type="text" id="ssd31">
@@ -148,7 +150,7 @@ return (
                     <input type="text" id="ssd71">
                     <input type="text" id="ssd81">
                 </div>
-                <div class="sharpScales">
+                <div className="sharpScales">
                     <input type="text" id="ssd12">
                     <input type="text" id="ssd22">
                     <input type="text" id="ssd32">
@@ -158,7 +160,7 @@ return (
                     <input type="text" id="ssd72">
                     <input type="text" id="ssd82">
                 </div>
-                <div class="sharpScales">
+                <div className="sharpScales">
                     <input type="text" id="ssd13">
                     <input type="text" id="ssd23">
                     <input type="text" id="ssd33">
@@ -168,7 +170,7 @@ return (
                     <input type="text" id="ssd73">
                     <input type="text" id="ssd83">
                 </div>
-                <div class="sharpScales">
+                <div className="sharpScales">
                     <input type="text" id="ssd14">
                     <input type="text" id="ssd24">
                     <input type="text" id="ssd34">
@@ -178,7 +180,7 @@ return (
                     <input type="text" id="ssd74">
                     <input type="text" id="ssd84">
                 </div>
-                <div class="sharpScales">
+                <div className="sharpScales">
                     <input type="text" id="ssd15">
                     <input type="text" id="ssd25">
                     <input type="text" id="ssd35">
@@ -188,7 +190,7 @@ return (
                     <input type="text" id="ssd75">
                     <input type="text" id="ssd85">
                 </div>
-                <div class="sharpScales">
+                <div className="sharpScales">
                     <input type="text" id="ssd15">
                     <input type="text" id="ssd25">
                     <input type="text" id="ssd35">
@@ -200,7 +202,7 @@ return (
                 </div>
             </div>
         </div>
-    </div>
+    {/* </div> */}
 </div>
 
 )};
