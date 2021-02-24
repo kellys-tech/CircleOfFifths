a8e738deb7c5cc3a9e9136ee379d437ec611631d
Kelly Smith <kdalton77@hotmail.com>
2021-02-23 19:37:50 -0500

    created components for flatscales, sharpscales, orderofflats, orderofsharps, circle

---
+++ client/src/components/flatscales/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+require ("../../App.css");
+
+function flatScales() {
+  return (
+  <div className="row">
+    <div className="col-3">
+        <div className="flatScales">
+                    <input type="text" id="fsd11"/>
+                    <input type="text" id="fsd12"/>
+                    <input type="text" id="fsd13"/>
+                    <input type="text" id="fsd14"/>
+                    <input type="text" id="fsd15"/>
+                    <input type="text" id="fsd16"/>
+                    <input type="text" id="fsd17"/>
+                    <input type="text" id="fsd18"/>
+                </div>
+                <div className="flatScales">
+                    <input type="text" id="fsd21"/>
+                    <input type="text" id="fsd22"/>
+                    <input type="text" id="fsd23"/>
+                    <input type="text" id="fsd24"/>
+                    <input type="text" id="fsd25"/>
+                    <input type="text" id="fsd26"/>
+                    <input type="text" id="fsd27"/>
+                    <input type="text" id="fsd28"/>
+                </div>
+                <div className="flatScales">
+                    <input type="text" id="fsd31"/>
+                    <input type="text" id="fsd32"/>
+                    <input type="text" id="fsd33"/>
+                    <input type="text" id="fsd34"/>
+                    <input type="text" id="fsd35"/>
+                    <input type="text" id="fsd36"/>
+                    <input type="text" id="fsd37"/>
+                    <input type="text" id="fsd38"/>
+                </div>
+                <div className="flatScales">
+                    <input type="text" id="fsd41"/>
+                    <input type="text" id="fsd42"/>
+                    <input type="text" id="fsd43"/>
+                    <input type="text" id="fsd44"/>
+                    <input type="text" id="fsd45"/>
+                    <input type="text" id="fsd46"/>
+                    <input type="text" id="fsd47"/>
+                    <input type="text" id="fsd48"/>
+                </div>
+                <div className="flatScales">
+                    <input type="text" id="fsd51"/>
+                    <input type="text" id="fsd52"/>
+                    <input type="text" id="fsd53"/>
+                    <input type="text" id="fsd54"/>
+                    <input type="text" id="fsd55"/>
+                    <input type="text" id="fsd56"/>
+                    <input type="text" id="fsd57"/>
+                    <input type="text" id="fsd58"/>
+                </div>
+                <div className="flatScales">
+                    <input type="text" id="fsd61"/>
+                    <input type="text" id="fsd62"/>
+                    <input type="text" id="fsd63"/>
+                    <input type="text" id="fsd64"/>
+                    <input type="text" id="fsd65"/>
+                    <input type="text" id="fsd66"/>
+                    <input type="text" id="fsd67"/>
+                    <input type="text" id="fsd68"/>
+                </div>
+                <div className="flatScales">
+                    <input type="text" id="fsd71"/>
+                    <input type="text" id="fsd72"/>
+                    <input type="text" id="fsd73"/>
+                    <input type="text" id="fsd74"/>
+                    <input type="text" id="fsd75"/>
+                    <input type="text" id="fsd76"/>
+                    <input type="text" id="fsd77"/>
+                    <input type="text" id="fsd78"/>
+                </div>
+    </div>
+                
+</div>
+
+)}
+
+export default flatScales;
